a33306beed89ce717d948ca65578d84bec6093fb
ALEN TL <alentl5000@gmail.com>
2021-03-17 17:02:05 +0530

    Fixed SQL

--- RashmikaBot/modules/sql/__init__.py
+++ RashmikaBot/modules/sql/__init__.py
@@ -1,8 +1,7 @@
+from RashmikaBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from RashmikaBot import DB_URI
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:

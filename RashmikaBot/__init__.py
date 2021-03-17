b8a035136c121815bd2b8936800cb50d26f4d79e
ALEN TL <alentl5000@gmail.com>
2021-03-17 16:07:19 +0530

    Added Support Chat

--- RashmikaBot/__init__.py
+++ RashmikaBot/__init__.py
@@ -84,7 +84,7 @@ if ENV:
     WALL_API = os.environ.get('WALL_API', None)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', False)
-    tbot = TelegramClient(None, API_KEY, API_HASH)
+    tbot = TelegramClient(API_KEY, API_HASH, None)
 
 else:
     from RashmikaBot.config import Development as Config

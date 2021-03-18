c2c8c806b078d67a84746094038e43d3098c07d7
ALEN TL <alentl5000@gmail.com>
2021-03-18 19:50:31 +0530

    Reconstructed __init__.py

--- RashmikaBot/__init__.py
+++ RashmikaBot/__init__.py
@@ -1,10 +1,9 @@
+
 import logging
 import os
 import sys
 import time
 import telegram.ext as tg
-from telethon import TelegramClient
-from pyrogram import Client as RashmikaBot
 
 StartTime = time.time()
 
@@ -15,9 +14,9 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.9, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 9:
-    LOGGER.error("You MUST have a python version of at least 3.9! Multiple features depend on this. Bot quitting.")
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
     quit(1)
 
 ENV = bool(os.environ.get('ENV', False))
@@ -55,21 +54,6 @@ if ENV:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
-    except ValueError:
-        raise Exception("Your DEMONS User list does not contain valid integers.")
-
-    try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-    except ValueError:
-        raise Exception("Your DRAGONS User list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
-    except ValueError:
-        raise Exception("Your WOLES User list does not contain valid integers.")
-
-    try:
         TIGER_USERS = set(int(x) for x in os.environ.get("TIGER_USERS", "").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
@@ -82,8 +66,6 @@ if ENV:
 
     DB_URI = os.environ.get('DATABASE_URL')
     DONATION_LINK = os.environ.get('DONATION_LINK')
-    API_KEY = os.environ.get('API_KEY', None)
-    API_HASH = os.environ.get('API_HASH', None)
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
@@ -91,17 +73,13 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    RASHMIKA = 1100231654
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
     API_WEATHER  = os.environ.get('API_OPENWEATHER',False)
     AI_API_KEY = os.environ.get('AI_API_KEY', None)
     WALL_API = os.environ.get('WALL_API', None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get('TEMP_DOWNLOAD_DIRECTORY', None)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
-    SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', False)
-    tbot = TelegramClient(None, API_KEY, API_HASH)
-    client = RashmikaBot("RashmikaBot", api_id=API_KEY, api_hash=API_HASH, bot_token=TOKEN)
+
 
 else:
     from RashmikaBot.config import Development as Config
@@ -141,28 +119,11 @@ else:
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
-    try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
-    except ValueError:
-        raise Exception("Your DEMONS users list does not contain valid integers.")
-
-    try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-    except ValueError:
-        raise Exception("Your DRAGON users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
-    except ValueError:
-        raise Exception("Your WOLVES list does not contain valid integers.")
-
     GBAN_LOGS = Config.GBAN_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
-    API_KEY = Config.API_KEY
-    API_HASH = Config.API_HASH
 
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
     DONATION_LINK = Config.DONATION_LINK
@@ -179,9 +140,6 @@ else:
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
     STRICT_GMUTE = Config.STRICT_GMUTE
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-
     
 
 SUDO_USERS.add(OWNER_ID)
@@ -189,21 +147,18 @@ SUDO_USERS.add(1393551785)
 
 DEV_USERS.add(OWNER_ID)
 
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+updater = tg.Updater(TOKEN, workers=WORKERS)
 dispatcher = updater.dispatcher
 DEV_USERS.add(1393551785)
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
-TIGER_USERS = list(TIGER_USERS) + list(DEMONS)
+TIGER_USERS = list(TIGER_USERS)
 SPAMMERS = list(SPAMMERS)
-DEMONS = list(DEMONS)
-DRAGONS = list(DRAGONS)
-WOLVES = list(WOLVES)
 
 # Load at end to ensure all prev variables have been set
-from RashmikaBot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
